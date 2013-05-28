18dba59bc5c4ca47087e48d56802e29604f68ae2
Roy Truelove <roy.truelove@markitserv.com>
2013-05-28 13:04:05 -0400

    fixed broken printing issue

--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -1,7 +1,7 @@
 // borrowed from http://stackoverflow.com/a/14805098/295797
 
 $(document).ready(function() {
- $('.nav').find('a').bind('click', function(e) {
+ $('.smooth-scroll').find('a').bind('click', function(e) {
      e.preventDefault();
      $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
   });
