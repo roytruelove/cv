e1ae840a5418055bbc4f04d72b40b8ebee04fdc0
Roy Truelove <roytruelove@gmail.com>
2023-02-16 14:39:13 +0100

    Trying to migrate HTML generation to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
-function defaultTask(cb) {
-  cb();
+import gulp from "gulp";
+//const { watch, series, src, dest } = require('gulp');
+//const {markdown} = require('gulp-markdown');
+
+const src_dir = "src/";
+
+function markdown(cb) {
+    cb();
 }
 
-exports.default = defaultTask
+// function sandbox(cb) {
+//     return src('src/*.md')
+//     .pipe(dest('output/'));
+// }
+
+// function watcher(cb) {
+//   watch('src/*.*', build);
+// }
+
+// function build(cb) {
+//   console.log("Building...");
+//   cb();
+// }
+
+function hello(cb) {
+    console.log("Hello World");
+    cb();
+}
+
+export default hello;
